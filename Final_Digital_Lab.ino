137b7206b9a4ac4a86804329829cbbd0052cd9f4
xanderzooi <44817727+xanderzooi@users.noreply.github.com>
2018-12-04 16:37:43 -0500

    Add files via upload

--- Final_Digital_Lab.ino
+++ Final_Digital_Lab.ino
@@ -4,14 +4,15 @@ int potPin3 = A7;
 int potPin4 = A6;
 int potArray[4] = {potPin, potPin2, potPin3, potPin4};
 
-
 int potValArray[4] = {0, 0, 0, 0};
 int lastPotValArray[4] = {0, 0, 0, 0};
 int ccVal[4] = {0, 0, 0, 0};
 int ccNumber[4] = {1, 2, 3, 4};
 
+int ccAdder = 0;
+int ccAdd = 0;
 
-int leftButton = 12;
+int leftButton = 14;
 boolean leftbuttonState = (LOW);
 boolean lastleftbuttonState = (LOW);
 int rightButton = 13;
@@ -20,10 +21,12 @@ boolean lastrightbuttonState = (LOW);
 
 unsigned long directionalButtonChange;
 
-
-int toggleButton = 14;
-boolean togglebuttonState = (LOW);
-boolean lasttogglebuttonState = (LOW);
+int lButton = 12;
+boolean lbuttonState = (LOW);
+boolean lastlbuttonState = (LOW);
+int rButton = 24;
+boolean rbuttonState = (LOW);
+boolean lastrbuttonState = (LOW);
 
 unsigned long lastCCtime[4] = {0, 0, 0, 0};
 
@@ -42,6 +45,8 @@ void setup() {
     pinMode(buttonArray[i], INPUT);
     pinMode(leftButton, INPUT);
     pinMode(rightButton, INPUT);
+    pinMode(lButton, INPUT);
+    pinMode(rButton, INPUT);
   }
   Serial.begin(9600);
 }
@@ -50,123 +55,93 @@ void setup() {
 void loop() {
   usbMIDI.read();
   potCheck();
-  activateControl();
+  buttonCheck();
 }
 
 void potCheck() {
-//  leftbuttonState = lastleftbuttonState;
-//  leftbuttonState = digitalRead(leftButton);
-//  rightbuttonState = lastrightbuttonState;
-//  rightbuttonState = digitalRead(rightButton);
+  lastleftbuttonState = leftbuttonState;
+  leftbuttonState = digitalRead(leftButton);
+  lastrightbuttonState = rightbuttonState;
+  rightbuttonState = digitalRead(rightButton);
 
+  if (rightbuttonState == HIGH && lastrightbuttonState == LOW) {
+    ccAdder = ccAdder + 4;
+  }
+
+  if (leftbuttonState == HIGH && lastleftbuttonState == LOW) {
+    ccAdder = ccAdder - 4;
+  }
+
+  if (ccAdder < 0) {
+    ccAdder = 0;
+  }
+  if (ccAdder > 120) {
+    ccAdder = 120;
+  }
 
   for (int i = 0; i < 4; i ++) {
-
-//     if (leftbuttonState == HIGH) {
-//     i = (i + 4);
-//    } 
-//  else if (rightbuttonState == HIGH) {
-//      i = (i - 4); }
-//      if (i < 0) {
-//        i = 0;
-//      }
-//      if ( i > 120); {
-//       i = 120;
-//      }
-
-      parameterControl (i);
-     }
-  
-  
-
+    parameterControl (i);
   }
-  
- 
-
-//void toggleEffects() {
-//  leftbuttonState = leftbuttonState;
-//  leftbuttonState = digitalRead(leftButton);
-//  
-//  
-// {
-//    
-//  }
-//}
+}
 
 
 
 void parameterControl (int ccNum) {
 
-
-  //Serial.println(ccNum);
-
-
   lastPotValArray[ccNum] = potValArray[ccNum];
   potValArray[ccNum] = analogRead(potArray[ccNum]) / 8;
+
   if ((potValArray[ccNum] != lastPotValArray[ccNum]) && millis() > lastCCtime[ccNum]) {
     lastCCtime[ccNum] = millis();
     ccVal[ccNum] = map(potValArray[ccNum], 0, 127, 0, 127);
-    usbMIDI.sendControlChange(ccNumber[ccNum], ccVal[ccNum], 1);
-    Serial.println(potValArray[ccNum]);
 
+    usbMIDI.sendControlChange(ccNumber[ccNum] + (ccAdder), ccVal[ccNum], 1);
+    Serial.println(potValArray[ccNum]);
     Serial.print(ccNum);
     Serial.print(", ");
     Serial.println(ccVal[ccNum]);
   }
 }
 
-void activateControl() {
+void buttonCheck() {
 
-  lastButtonStateArray[0] = buttonStateArray[0];
-  buttonStateArray[0] = digitalRead(buttonArray[0]);
+  lastlbuttonState = lbuttonState;
+  lbuttonState = digitalRead(lButton);
+  lastrbuttonState = rbuttonState;
+  rbuttonState = digitalRead(rButton);
 
-  if (buttonStateArray[0] == HIGH && lastButtonStateArray[0] == LOW) {
-    if (on == false) {
-      on = true;
-      usbMIDI.sendNoteOn (1, 127, 1);
-    } else if (on == true) {
-      on = false;
-      usbMIDI.sendNoteOff (1, 0, 1);
-    }
+  if (rbuttonState == HIGH && lastrbuttonState == LOW) {
+    ccAdd = ccAdd + 4;
   }
 
-  lastButtonStateArray[1] = buttonStateArray[1];
-  buttonStateArray[1] = digitalRead(buttonArray[1]);
-  if (buttonStateArray[1] == HIGH && lastButtonStateArray[1] == LOW) {
-    if (on == false) {
-      on = true;
-      usbMIDI.sendNoteOn (2, 126, 1);
-    } else if (on == true) {
-      on = false;
-      usbMIDI.sendNoteOff (2, 0, 1);
-    }
+  if (lbuttonState == HIGH && lastlbuttonState == LOW) {
+    ccAdd = ccAdd - 4;
   }
-
-
-  lastButtonStateArray[2] = buttonStateArray[2];
-  buttonStateArray[2] = digitalRead(buttonArray[2]);
-
-  if (buttonStateArray[2] == HIGH && lastButtonStateArray[2] == LOW) {
-    if (on == false) {
-      on = true;
-      usbMIDI.sendNoteOn (3, 125, 1);
-    } else if (on == true) {
-      on = false;
-      usbMIDI.sendNoteOff (3, 0, 1);
-    }
+  if (ccAdd < 0) {
+    ccAdd = 0;
   }
+  if (ccAdd > 120) {
+    ccAdd = 120;
+  }
+  for (int i = 0; i < 4; i ++) {
+    activateControl (i);
+  }
+}
 
-  lastButtonStateArray[3] = buttonStateArray[3];
-  buttonStateArray[3] = digitalRead(buttonArray[3]);
 
-  if (buttonStateArray[3] == HIGH && lastButtonStateArray[3] == LOW) {
+
+void activateControl(int ccNum) {
+
+  lastButtonStateArray[ccNum] = buttonStateArray[ccNum];
+  buttonStateArray[ccNum] = digitalRead(buttonArray[ccNum]);
+
+  if (buttonStateArray[ccNum] == HIGH && lastButtonStateArray[ccNum] == LOW) {
     if (on == false) {
       on = true;
-      usbMIDI.sendNoteOn (4, 124, 1);
+      usbMIDI.sendNoteOn (ccNumber[ccNum] + (ccAdd), 127, 1);
     } else if (on == true) {
       on = false;
-      usbMIDI.sendNoteOff (4, 0, 1);
-
+      usbMIDI.sendNoteOff (ccNumber[ccNum] + (ccAdd), 0, 1);
     }
   }
 }
